6be8eaab8b82ca691c31ab7a6d74bd1cab0127cd
Aayush1720 <choubeyaayush1720@gmail.com>
2021-05-22 01:55:36 +0530

    completed the algorithm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# btw this example which i am taking is considered to be the world'es toguhest sudoku problem.
 board = [
     [8,0,0,0,0,0,0,0,0],
     [0,0,3,6,0,0,0,0,0],
@@ -10,6 +11,7 @@ board = [
     [0,9,0,0,0,0,4,0,0]
 ]
 
+# this function basically checks whether placing number n on the ith row and jth column is legal or not!
 def checker(i , j, n):
     for k in range(0,9):
         if board[k][j] == n:
@@ -24,3 +26,34 @@ def checker(i , j, n):
             if board[k][l] == n:
                 return False
     return True
+# this function gives the next vacant cell
+def get_vacant():
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i,j)
+    return None
+# this is a recursive function based on the idea of baktracking. 
+def solver():
+    next = get_vacant()
+    if not next:
+        return True
+    x,y = next
+    for i in range(1,10):
+        if checker(x,y,i):
+            board[x][y] = i
+            if solver():
+                return True
+            board[x][y] = 0
+    return False            
+
+
+solved = solver()
+if not solved :
+    print("wrong sudoku!")
+
+# finally printing the answer
+for i  in range(9):
+    for j in range(9):
+        print(board[i][j],end="")
+    print("\n",end = "")
